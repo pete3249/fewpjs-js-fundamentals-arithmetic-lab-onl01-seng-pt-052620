0bfc733e13daf511c521f838c1883420da89641f
pete3249 <katelynrpeterson16@gmail.com>
2020-12-14 10:45:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2
+
+let multiply = num1 * num2
+
+let random = Math.random();
+
+let mod = 10 % 6;
+
+let max = Math.max(19, 18, 17, 16, 15, 14, 20);
